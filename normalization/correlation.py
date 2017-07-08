c07a43e20478ad34fbc65ee1a79f280069050cbf
Jasdeep Singh Chhabra <jasdeepchhabra94@gmail.com>
2017-07-08 18:25:35 +0530

    Update correlation.py

--- normalization/correlation.py
+++ normalization/correlation.py
@@ -1,34 +1,29 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+
+#reading data
 data=pd.read_csv("dataset/tc_house_price.csv")
 
-
+#missing data removal
 missing_lot=(data.loc[data["LotSize"].isnull()]).index
 data.drop(missing_lot, axis=0, inplace=True)
 missing_bath = (data.loc[data["BATHS"].isnull()]).index
 data.drop(missing_bath, axis=0, inplace=True)
 
-
+#numeric features for centering
 numeric_features=data.select_dtypes(include=[np.number])
+
+#squared features centering
 for feature in numeric_features:
     data[feature+"Squared"]=data[feature]**2
     data[feature+"Centered"]=data[feature]-(data[feature].mean())
     data[feature+"CenteredSquared"]=data[feature+"Centered"]**2
+    
+    #printing correlations between (feature,squared feature) and (centered feature,centered squared feature)
     print(feature+","+ feature +"Squared",data[feature].corr(data[feature+"Squared"]))
     print(feature + "Centered," + feature + "CenteredSquared", data[feature+"Centered"].corr(data[feature + "CenteredSquared"]))
 
 
 
 
-plt.scatter(data["DOMCentered"],data["DOMCenteredSquared"],label="DOM")
-#plt.hist(data["DOMCenteredSquared"])
-#plt.hist(data["BEDSCentered"])
-plt.show()
-print(data["LotSize"].var())
-data["LotSizeSquared"]=data["LotSize"]**2
-data["LotSizeCentered"]=data["LotSize"]-(data["LotSize"].mean())
-data["LotSizeCenteredSquared"]=data["LotSizeCentered"]**2
-
-print(data["LotSize"].corr(data["LotSizeSquared"]))
-print(data["LotSizeCentered"].corr(data["LotSizeCenteredSquared"]))
